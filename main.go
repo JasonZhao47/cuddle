e30a42eb59ac1b1fdde8ab3434eb0e535e55d969
-=[ Heisenberg ]=- <roastedcookie47@hotmail.com>
2023-11-14 16:54:26 +0800

    feature: configuration with viper

--- main.go
+++ main.go
@@ -3,26 +3,41 @@ package main
 import (
 	"fmt"
 	"github.com/gin-gonic/gin"
+	"github.com/spf13/pflag"
 	"github.com/spf13/viper"
 	"net/http"
 )
 
 func main() {
 	// all-in-one initialization for web server
-	initViper()
+	initViperV2()
+	//initLogger()
+	//initThirdParty()
 	server := InitWebServer()
 	// run a health check
 	server.GET("/health", func(ctx *gin.Context) {
 		ctx.String(http.StatusOK, "I'm still alive!")
 	})
 	// run on a port
-	err := server.Run(viper.GetString("server.http.addr"))
+	type config struct {
+		Addr string `yaml:"addr"`
+	}
+	var c config
+	c = config{
+		Addr: ":8080",
+	}
+	err := viper.UnmarshalKey("server.http", &c)
+	if err != nil {
+		panic(err)
+	}
+	err = server.Run(c.Addr)
 	if err != nil {
 		panic(err)
 	}
 	// shutdown the server gracefully
 }
 
+// Deprecated: Use V2 instead
 func initViper() {
 	// load in minimum configs
 	// including server itself
@@ -33,5 +48,15 @@ func initViper() {
 	err := viper.ReadInConfig()
 	if err != nil {
 		panic(fmt.Errorf("fatal error config file: %w", err))
+	}
+}
+
+func initViperV2() {
+	cfile := pflag.String("config", "config/dev.yaml", "path to config")
+	pflag.Parse()
+	viper.SetConfigFile(*cfile)
+	err := viper.ReadInConfig()
+	if err != nil {
+		panic(err)
 	}
 }
